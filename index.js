64d832317c1eb2ba18269ebd7066b5146ce7c150
Aishwarya1607 <139792520+Aishwarya1607@users.noreply.github.com>
2023-10-22 13:57:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors'; 
-import dotenv from 'dotenv';
-import postRoutes from './routes/posts.js';
-import userRoutes from './routes/user.js';
-import path from 'path';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+const path = require('path');
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.static(path.join(__dirname,"./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build"));
 
-app.use("*", function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/App.js"));
-})
-
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/App.js"));
+});
 
 app.use(cors({
-  origin: ['http://localhost:3000'], 
+  origin: ['http://localhost:3000'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
 }));
 
+const PORT = process.env.PORT || 5000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Connected`);
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+}
+
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+});
