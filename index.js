17e413d19bcfd93c27cbe3dca11446abbf11b5e0
Aishwarya1607 <139792520+Aishwarya1607@users.noreply.github.com>
2023-10-22 14:38:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/user.js';
 import path from 'path';
-const path = require('path');
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
