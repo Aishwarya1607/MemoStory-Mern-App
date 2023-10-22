5fba7d0846712b0a955a96d3166ab9a6fe0d73de
Aishwarya1607 <139792520+Aishwarya1607@users.noreply.github.com>
2023-10-22 14:54:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors'; 
+import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/user.js';
 import path from 'path';
+
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
 dotenv.config();
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.static(path.join(__dirname,"./client/build")));
-
-app.use("*", function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/App.js"));
-})
-
 
 app.use(cors({
   origin: ['http://localhost:3000'], 
@@ -27,11 +20,28 @@ app.use(cors({
   allowedHeaders: 'Content-Type,Authorization',
 }));
 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
+
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
